d1c3ed9e0b45b1c0c7a3a264c94476c20fd318f8
Oriol Burgaya <oriolbv@gmail.com>
2020-03-25 22:52:33 +0100

    Jump improved

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,13 +17,22 @@ public class Player : MonoBehaviour
     [Header("Physics")]
     public float maxSpeed = 7f;
     public float linearDrag = 4f;
+    public float jumpSpeed = 3f;
+    public float gravity = 1;
+    public float fallMultiplier = 5f;
 
     [Header("Collision")]
     public bool onGround = false;
+    public float groundLength = 0.6f;
 
     void Update()
     {
-        onGround = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, groundLayer);
+        onGround = Physics2D.Raycast(transform.position, Vector2.down, groundLength, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && onGround) 
+        {
+            Jump();
+        }
 
         // Left direction: -1 | Idle: 0 | Right direction: 1
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
@@ -52,23 +61,53 @@ public class Player : MonoBehaviour
         }
     }
 
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+    }
+
     void modifyPhysics()
     {
         bool changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
 
-        if (Mathf.Abs(direction.x) < 0.4f || changingDirections) 
+        if (onGround) 
         {
-            rb.drag = linearDrag;
+            if (Mathf.Abs(direction.x) < 0.4f || changingDirections) 
+            {
+                rb.drag = linearDrag;
+            }
+            else
+            {
+                rb.drag = 0f;
+            }
+            rb.gravityScale = 0;
         }
-        else
+        else 
         {
-            rb.drag = 0f;
+            rb.gravityScale = gravity;
+            rb.drag = linearDrag * 0.15f;
+            if (rb.velocity.y < 0) 
+            {
+                rb.gravityScale = gravity * fallMultiplier;
+            }
+            else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) 
+            {
+                rb.gravityScale = gravity * (fallMultiplier / 2);
+            }
         }
+        
     }
 
     void Flip() 
     {
         movingRight = !movingRight;
         transform.rotation = Quaternion.Euler(0, movingRight ? 0 : 180, 0);
+    }
+
+    void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundLength);    
     }
 }
