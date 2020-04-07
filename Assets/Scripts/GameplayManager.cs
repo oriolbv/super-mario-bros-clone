41de80f2f64eb130214712801e192c2db8a5b61c
Oriol Burgaya <oriolbv@gmail.com>
2020-04-07 20:53:30 +0200

    Score data structre and singleton class added

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameplayManager : MonoBehaviour
+public class GameplayManager : Singleton<GameplayManager>
 {
+    [Header("Game Score")]
+    private GameScore _score;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _score = new GameScore(0, 0, 360);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _score.RemainingTime -= Time.deltaTime;
+        if (_score.RemainingTime < 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver() 
+    {
+        Debug.Log("This is the end");
+    }
+
+
+    // Properties
+    public GameScore Score
+    {
+        get 
+        { 
+            return _score; 
+        }
+        set 
+        { 
+            _score = value; 
+        }
     }
 }
---
+++ Assets/Scripts/Models/GameScore.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScore
+{
+    private int _score;
+    private int _coins;
+    private float _remainingTime;
+
+    public GameScore(int score, int coins, float remainingTime) 
+    {
+        _score = score;
+        _coins = coins;
+        _remainingTime = remainingTime;
+    }
+
+    // Properties
+    public int Score
+    {
+        get 
+        { 
+            return _score; 
+        }
+        set 
+        { 
+            _score = value; 
+        }
+    }
+
+    public int Coins
+    {
+        get 
+        { 
+            return _coins; 
+        }
+        set 
+        { 
+            _coins = value; 
+        }
+    }
+
+    public float RemainingTime
+    {
+        get 
+        { 
+            return _remainingTime; 
+        }
+        set 
+        { 
+            _remainingTime = value; 
+        }
+    }
+}
