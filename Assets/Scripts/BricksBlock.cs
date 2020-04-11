313992a7eeb5227cde2cb3af3907eb3a4697db45
Oriol Burgaya <oriolbv@gmail.com>
2020-04-11 23:08:53 +0200

    Blocks behaviour implemented

--- Assets/Scripts/BricksBlock.cs
+++ Assets/Scripts/BricksBlock.cs
@@ -7,25 +7,20 @@ using UnityEngine.Tilemaps;
 public class BricksBlock : MonoBehaviour
 {
     private bool _isSuperMario;
-    private Tilemap tilemap;
 
     private void Start()
     {
         // TODO: Super Mario behaviour
         _isSuperMario = true;
-        tilemap = this.GetComponentInChildren<Tilemap>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector3 hitPosition = Vector3.zero;
         if (collision.collider.GetComponent<Player>() && collision.contacts[0].normal.y > 0.5f)
         {
             if (_isSuperMario)
             {
-                hitPosition.x = (float)Math.Truncate(collision.contacts[0].point.x);
-                hitPosition.y = (float)Math.Truncate(collision.contacts[0].point.y);
-                tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+                Destroy(this.gameObject);
             }
         }
     }
--- Assets/Scripts/CoinBlock.cs
+++ Assets/Scripts/CoinBlock.cs
@@ -7,36 +7,18 @@ using UnityEngine.Tilemaps;
 public class CoinBlock : ExtendedBehaviour
 {
     private bool _isCoinBlockInitialState;
-    public Sprite newSprite;
-    private Tile myTile;
-    private Tilemap tilemap;
+    public Sprite UsedBlockSprite;
 
     void Start()
     {
-        
         _isCoinBlockInitialState = true;
-        tilemap = this.GetComponentInChildren<Tilemap>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector3 hitPosition = Vector3.zero;
         if (collision.collider.GetComponent<Player>() && collision.contacts[0].normal.y > 0.5f)
         {
-            myTile = new Tile();
-            if (_isCoinBlockInitialState)
-            {
-                hitPosition.x = (float)Math.Truncate(collision.contacts[0].point.x);
-                hitPosition.y = (float)Math.Truncate(collision.contacts[0].point.y);
-
-                Debug.Log(myTile.sprite);
-                Debug.Log(newSprite);
-                if (myTile.sprite != newSprite)
-                {
-                    myTile.sprite = newSprite;
-                    tilemap.SetTile(tilemap.WorldToCell(hitPosition), myTile);
-                }
-            }
+            this.GetComponentInChildren<SpriteRenderer>().sprite = UsedBlockSprite;
         }
     }
 }
