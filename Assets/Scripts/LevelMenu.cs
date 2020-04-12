257716c33ea57063dd691624fa8d17b29f21d176
Oriol Burgaya <oriolbv@gmail.com>
2020-04-12 23:34:46 +0200

    GUI menus transitions implemented

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : ExtendedBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Wait(2f, () => {
+            SceneManager.LoadScene("Level11Scene");
+        }); 
+    }
+}
