b00b710db05647a4725dd2ae4581c1bcfd3790c6
Oriol Burgaya <oriolbv@gmail.com>
2020-04-12 20:16:54 +0200

    BricksBlock animation added

--- Assets/Scripts/BricksBlock.cs
+++ Assets/Scripts/BricksBlock.cs
@@ -4,23 +4,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class BricksBlock : MonoBehaviour
+public class BricksBlock : ExtendedBehaviour
 {
     private bool _isSuperMario;
+
+    private Animator animator;
 
     private void Start()
     {
         // TODO: Super Mario behaviour
-        _isSuperMario = true;
+        _isSuperMario = false;
+        animator = this.GetComponentInChildren<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.GetComponent<Player>() && collision.contacts[0].normal.y > 0.5f)
         {
+            animator.SetTrigger("useBlock");
             if (_isSuperMario)
             {
-                Destroy(this.gameObject);
+                Wait(0.2f, () => {
+                    Destroy(this.gameObject);
+                });
             }
         }
     }
