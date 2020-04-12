8f1f4df2841a3191a35f8807493fa084711c5ff0
Oriol Burgaya <oriolbv@gmail.com>
2020-04-12 13:59:54 +0200

    Coin block animation implemented

--- Assets/Scripts/CoinBlock.cs
+++ Assets/Scripts/CoinBlock.cs
@@ -9,20 +9,25 @@ public class CoinBlock : ExtendedBehaviour
     private bool _isCoinBlockInitialState;
     public Sprite UsedBlockSprite;
 
+
+    private Animator animator;
+
     void Start()
     {
         _isCoinBlockInitialState = true;
+        animator = this.GetComponentInChildren<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.GetComponent<Player>() && collision.contacts[0].normal.y > 0.5f)
         {
-            this.GetComponentInChildren<SpriteRenderer>().sprite = UsedBlockSprite;
-            this.transform.Translate(new Vector3(0, 0.2f, 0));
-            Wait(0.1f, () => {
-                this.transform.Translate(new Vector3(0, -0.2f, 0));
-            });
+            if (_isCoinBlockInitialState)
+            {
+                _isCoinBlockInitialState = false;
+                this.GetComponentInChildren<SpriteRenderer>().sprite = UsedBlockSprite;
+                animator.SetTrigger("useBlock");
+            }
         }
     }
 }
