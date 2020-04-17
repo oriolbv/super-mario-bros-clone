a6c0f1054acd6b1d8686bdebf5e81ab6cacec36a
Oriol Burgaya <oriolbv@gmail.com>
2020-04-17 17:42:59 +0200

    Goomba change direction when collission between them

--- Assets/Scripts/Goomba.cs
+++ Assets/Scripts/Goomba.cs
@@ -33,7 +33,7 @@ public class Goomba : ExtendedBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Wall"))
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Enemy"))
         {
             MoveRight = !MoveRight;
         }
