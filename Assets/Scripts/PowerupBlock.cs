f6e10dd9fc87e06cf12722d379e1d333682f922a
Oriol Burgaya <oriolbv@gmail.com>
2020-04-17 14:22:49 +0200

    PowerupBlock prefab and script behaviour

---
+++ Assets/Scripts/PowerupBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupBlock : MonoBehaviour
+{
+    private bool _isPowerupBlockInitialState;
+    public Sprite UsedBlockSprite;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isPowerupBlockInitialState = true;
+        animator = this.GetComponentInChildren<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Player>() && collision.contacts[0].normal.y > 0.5f)
+        {
+            if (_isPowerupBlockInitialState)
+            {
+                _isPowerupBlockInitialState = false;
+                this.GetComponentInChildren<SpriteRenderer>().sprite = UsedBlockSprite;
+                animator.SetTrigger("useBlock");
+            }
+        }
+    }
+}
