15b8b4d93e0f27cac98771f8d0d16831bcaa980c
Oriol Burgaya <oriolbv@gmail.com>
2020-05-08 20:57:46 +0200

    FlowerBlock script implemented

---
+++ Assets/FlowerBlock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerBlock : MonoBehaviour
+{
+    private bool _isFlowerBlockInitialState;
+    public Sprite UsedBlockSprite;
+
+    private AudioSource blockAudioSource;
+
+    private Animator animator;
+
+    void Start()
+    {
+        _isFlowerBlockInitialState = true;
+        animator = this.GetComponentInChildren<Animator>();
+
+        blockAudioSource = this.GetComponentInChildren<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Player>() && collision.contacts[0].normal.y > 0.5f)
+        {
+            if (_isFlowerBlockInitialState)
+            {
+                _isFlowerBlockInitialState = false;
+                this.GetComponentInChildren<SpriteRenderer>().sprite = UsedBlockSprite;
+                animator.SetTrigger("useBlock");
+                // Reproduce sound
+                blockAudioSource.Play();
+            }
+        }
+    }
+}
